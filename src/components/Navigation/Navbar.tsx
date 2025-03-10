7d0ff2cda05a0eae2b7770f07565def7fac455de
Chinna Kadinti <157778675+Chinna-Kadinti@users.noreply.github.com>
2025-03-10 21:51:06 +0530

    Update Navbar.tsx
    Updated Navbar component

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -42,63 +42,78 @@ const Navbar: React.FC = () => {
   };
   
   return (
-    <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-      <Toolbar>
-        <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-          <img 
-            src={logo} 
-            alt="GSynergy Logo" 
-            style={{ height: '40px', marginRight: '16px' }} 
+    <AppBar 
+    position="fixed" 
+    sx={{ 
+      zIndex: (theme) => theme.zIndex.drawer + 1,
+      backgroundColor: 'white',  // Set background color to white
+      color: 'black',           // Set default text color to black
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' // Optional: subtle shadow
+    }}
+  >
+    <Toolbar>
+      <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center'}}>
+        <img 
+          src={logo} 
+          alt="GSynergy Logo" 
+          style={{ height: '40px', marginRight: '16px' }} 
+        />
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center', color: 'black' }}>
+          Data Viewer App
+        </Typography>
+      </Box>
+      
+      {isAuthenticated && (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <FileUploader />
+          <Divider 
+            orientation="vertical" 
+            flexItem 
+            sx={{ mx: 2, backgroundColor: 'rgba(0,0,0,0.3)' }} // Dark divider
           />
-          <Typography variant="h6" component="div">
-            Data Viewer App
-          </Typography>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            sx={{ color: 'black' }} // Icon color
+          >
+            <AccountCircle />
+            <Typography variant="body1" sx={{ ml: 1, color: 'black' }}>
+              {username}
+            </Typography>
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleLogout}>Sign Out</MenuItem>
+          </Menu>
         </Box>
-        
-        {isAuthenticated && (
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <FileUploader />
-            <Divider orientation="vertical" flexItem sx={{ mx: 2, backgroundColor: 'rgba(255,255,255,0.3)' }} />
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-              <Typography variant="body1" sx={{ ml: 1 }}>
-                {username}
-              </Typography>
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleLogout}>Sign Out</MenuItem>
-            </Menu>
-          </Box>
-        )}
-        
-        {!isAuthenticated && (
-          <Button color="inherit" onClick={() => navigate('/login')}>
-            Sign In
-          </Button>
-        )}
-      </Toolbar>
-    </AppBar>
+      )}
+      
+      {!isAuthenticated && (
+        <Button 
+          sx={{ color: 'black' }} // Black text for sign-in button
+          onClick={() => navigate('/login')}
+        >
+          Sign In
+        </Button>
+      )}
+    </Toolbar>
+  </AppBar>
   );
 };
 
